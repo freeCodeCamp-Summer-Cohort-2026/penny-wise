1d861f525c43ca2bd48551c54ad186fc38139080
Sebastian Włodarczyk <sebastianwlodarczyk92@protonmail.com>
2026-09-02 16:04:05 +0200

    build: add MongoDB Docker Container
    This adds a docker compose file containing MongoDB 8.2 to the project, and modifies the environmental MONGODB_URI variable to use the correct port for the connection.

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
+
+require('dotenv').config({ path: '../.env' });
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 //Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
+mongoose
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(Server running on port ${PORT}));
-
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
