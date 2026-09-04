94ba974c445b44616d2ad819ee6c5f1110899c62
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-09-04 17:40:11 -0400

    feat: create pages folder and file for about page

---
+++ frontend/src/pages/AboutPage.jsx
@@ -0,0 +1,14 @@
+// DELETE ME LATER
+// page layout should be very basic, keeping in line w/ skeleton concept
+// go off of this for inspo: https://res.cloudinary.com/time2hack/image/upload/layout.png
+// top has a greyed "banner" to act as an image placeholder
+// h1 for title below
+// grey rounded body w/ flexbox trio of boxes largely labelled "1 2 3" for brief sections (likely about penny-wise's goals)
+
+function AboutPage() {
+    return (
+        <></>
+    )
+}
+
+export default AboutPage
