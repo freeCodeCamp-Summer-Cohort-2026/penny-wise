52d2f6a111e217b9da29f41840e6adf6aceb9bcb
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-09-07 15:39:27 -0400

    feat: add tailwind styling

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -2,22 +2,22 @@ function AboutPage() {
     return (
         <>
         {/* NavBar would go here */}
-            <main>
+        <main>
                 <h1>About Us</h1>
                 <section>
                     <p>The pennies behind the wisdom.</p>
-                    <div>
-                        <div>
+                    <div className="flex flex-col sm:flex-row justify-around content-center gap-3 my-8">
+                        <div className="border flex-initial basis-md h-24 rounded-md hover:shadow-xl duration-100">
                             <h2>1</h2>
-                            <p>Lorem</p>
+                            <p className="text-xl">Lorem</p>
                         </div>
-                        <div>
+                        <div className="border flex-initial basis-md h-24 rounded-md hover:shadow-xl duration-100">
                             <h2>2</h2>
-                            <p>Ipsum</p>
+                            <p className="text-xl">Ipsum</p>
                         </div>
-                        <div>
+                        <div className="border flex-initial basis-md h-24 rounded-md hover:shadow-xl duration-100">
                             <h2>3</h2>
-                            <p>Dolor</p>
+                            <p className="text-xl">Dolor</p>
                         </div>
                     </div>
                 </section>
